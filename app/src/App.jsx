f569a165ec1e5492bd04c81ea73b226df5663b08
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2024-05-11 01:46:56 +0100

    fix: card design

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Segmented, Tooltip } from "antd";
 import Card from "./components/Card";
+import { MdInfoOutline } from "react-icons/md";
 
 export function parseDate(dateString) {
   const parsedDate = new Date(Date.parse(dateString));
@@ -37,90 +38,93 @@ function App() {
   const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
-    <>
-      <div className="container flex flex-col font-sans p-5">
-        {/* heading */}
-        <h1 className="font-bold text-3xl">Economic Profile</h1>
-        <p>Select a different country to view its economic indicators</p>
+    <div className="container flex flex-col font-sans p-8">
+      {/* heading */}
+      <h1 className="font-bold text-3xl">Economic Profile</h1>
+      <p>Select a different country to view its economic indicators</p>
 
-        {/* select component */}
-        <select name="Select country" id=""></select>
+      {/* select component */}
+      <select name="Select country" id=""></select>
 
-        {/* country details */}
-        <h2 className="text-2xl font-bold mt-5">{capitalize(country)}</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          ducimus sunt odio saepe animi, qui amet optio aut pariatur
-          necessitatibus assumenda perspiciatis iste voluptatum. Veniam illum
-          commodi quibusdam ipsa delectus.
-        </p>
+      {/* country details */}
+      <h2 className="text-2xl font-bold mt-5">{capitalize(country)}</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ducimus
+        sunt odio saepe animi, qui amet optio aut pariatur necessitatibus
+        assumenda perspiciatis iste voluptatum. Veniam illum commodi quibusdam
+        ipsa delectus.
+      </p>
 
-        {/* macro-economic indicators */}
-        <div className="flex mt-5">
-          <h3 className="text-xl font-bold">Macro-Economic Indicators</h3>
-          <Tooltip>
-            <img src="/info.svg" alt="info" />
-          </Tooltip>
-        </div>
-
-        <div className="flex flex-col md:flex-row gap-5">
-          <Card
-            title={"GDP"}
-            content={"$4.2 Trillion"}
-            foot={"-3.2%"}
-            tooltip={
-              "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
-            }
-          />
-
-          <Card
-            title={"GDP"}
-            content={"$4.2 Trillion"}
-            foot={"-3.2%"}
-            tooltip={
-              "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
-            }
-          />
-
-          <Card
-            title={"GDP"}
-            content={"$4.2 Trillion"}
-            foot={"-3.2%"}
-            tooltip={
-              "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
-            }
-          />
-        </div>
-
-        {/* trade charts */}
-        <div className="flex mt-5">
-          <h3 className="text-xl font-bold">Trade</h3>
-          <Tooltip title="Imports and exports of goods by category">
-            <img src="/info.svg" alt="info" />
-          </Tooltip>
-        </div>
-
-        <Segmented options={["Imports by category", "Exports by category"]} />
-        {/* chart here */}
-
-        <div className="flex mt-5">
-          <h3 className="text-xl font-bold">Latest News</h3>
-          <Tooltip>
-            <img src="/info.svg" alt="info" />
-          </Tooltip>
-        </div>
-
-        <div className="container flex flex-col gap-5">
-          {news.map((item, index) => (
-            <>
-              {/* <div key={index}>{parseDate(item.date)}</div>
-          <div>{item.title}</div> */}
-              <Card key={index} title={parseDate(item.date)} content={item.title} />
-            </>
-          ))}
-        </div>
+      {/* macro-economic indicators */}
+      <div className="flex mt-5 gap-1 items-center">
+        <h3 className="text-xl font-bold">Macro-Economic Indicators</h3>
+        <Tooltip>
+          <MdInfoOutline />
+        </Tooltip>
       </div>
-    </>
+
+      {/* macro-economic cards */}
+      <div className="flex flex-col md:flex-row gap-5">
+        <Card
+          title={"GDP"}
+          content={"$4.2 Trillion"}
+          foot={"-3.2%"}
+          tooltip={
+            "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
+          }
+        />
+
+        <Card
+          title={"GDP"}
+          content={"$4.2 Trillion"}
+          foot={"-3.2%"}
+          tooltip={
+            "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
+          }
+        />
+
+        <Card
+          title={"GDP"}
+          content={"$4.2 Trillion"}
+          foot={"-3.2%"}
+          tooltip={
+            "Gross domestic product is a monetary measure of the market value of all the final goods and services produced and rendered in a specific time period by a country or countries"
+          }
+        />
+      </div>
+
+      {/* trade charts */}
+      <div className="flex mt-5 gap-1 items-center">
+        <h3 className="text-xl font-bold">Trade</h3>
+        <Tooltip title="Imports and exports of goods by category">
+          <MdInfoOutline />
+        </Tooltip>
+      </div>
+
+      <Segmented options={["Imports by category", "Exports by category"]} />
+      {/* chart here */}
+
+      <div className="flex mt-5 gap-1 items-center ">
+        <h3 className="text-xl font-bold">Latest News</h3>
+        <Tooltip>
+          <MdInfoOutline />
+        </Tooltip>
+      </div>
+
+      <div className="container flex flex-col gap-5">
+        {news.map((item, index) => (
+          <>
+            {/* <div key={index}>{parseDate(item.date)}</div>
+          <div>{item.title}</div> */}
+            <Card
+              key={index}
+              title={parseDate(item.date)}
+              content={<a href={item.url}>{item.title}</a>}
+            />
+          </>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,19 +1,19 @@
 import { Tooltip } from "antd";
 import { MdInfoOutline } from "react-icons/md";
 
-function Card({ title, content, foot, tooltip }) {
+function Card({ title, content, foot, tooltip}) {
   return (
-    <div className="flex flex-col justify-between bg-black text-white p-3 rounded-xl min-w-32 sm:min-w-10 min-h-32">
+    <div className="flex flex-col justify-between bg-black px-5 text-white p-3 rounded-xl min-w-32 sm:min-w-10 gap-2 ">
       <div className="flex justify-between">
-        <span className="text-sm">{title}</span>
+        <span className="text-sm text-trade-green">{title}</span>
         {tooltip && (
           <Tooltip title={tooltip}>
             <MdInfoOutline />
           </Tooltip>
         )}
       </div>
-      <p className="text-2xl">{content}</p>
-      <p className="text-md">{foot}</p>
+      <p className="text-md">{content}</p>
+      {foot && <div className="text-sm self-end">{foot}</div>}
     </div>
   );
 }
